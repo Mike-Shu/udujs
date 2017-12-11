b017cc61fc300e0ea5bf8c3bddebc4b7da50d46c
mike_shu <heliax44@gmail.com>
2017-12-12 02:43:03 +0400

    Added Server-app file.

--- app/server-app.js
+++ app/server-app.js
@@ -1,1 +1,26 @@
-console.log('Initializing the project.');
+// Dependencies.
+const UduJS = require('../src');
+const Misc = require('./misc');
+
+const Debug = new UduJS({
+  consoleEOL: '\r\n',
+});
+const someCode = Misc.doSomeCode;
+
+// View the value of some variable.
+Debug.log(Misc.multiObject);
+
+// Measurement with control points.
+Debug.rttPoint();
+someCode();
+Debug.rttPoint('Some code was executed.');
+someCode();
+Debug.rttPoint('More code.');
+
+// A single measurement.
+Debug.rttStart('Single testing of some code.');
+someCode();
+Debug.rttFinish();
+
+// A single measurement with the calculation of the average result.
+Debug.rttAverage(someCode, 10, 'The average execution time of some code.', true);
