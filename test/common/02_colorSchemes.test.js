242ed70c2ce93fecc4c987f7eb0ac7ba70d56562
mike_shu <heliax44@gmail.com>
2017-12-12 03:17:49 +0400

    Added а test set for the colorSchemes file.

---
+++ test/common/02_colorSchemes.test.js
@@ -0,0 +1,93 @@
+// Dependencies.
+const mocha = require('mocha');
+
+// Module for testing and helper set.
+const colorSchemes = require('../../src/lib/colorSchemes');
+const Utility = require('../misc/utility');
+
+// Tools.
+const Describe = mocha.describe;
+const It = mocha.it;
+
+// Test management: useful for debugging to enable or disable test sets.
+Utility.managementFor('server');
+const testManagement = Utility.testManagement([
+  'fileExists',
+  'schemes',
+]);
+
+Utility.run('colorSchemes', () => {
+  Describe(Utility.name('Set of color schemes'), () => {
+    testManagement.run('fileExists', () => {
+      It('The file exists and contains the object.', () => {
+        colorSchemes.should.is.an('Object');
+      });
+    });
+    testManagement.run('schemes', () => {
+      const schemePattern = {
+        console: {
+          schemes: [
+            'dark',
+            'bright',
+            'custom',
+          ],
+          properties: [
+            'heading',
+            'master',
+            'slave',
+            'attention',
+          ],
+        },
+        popup: {
+          schemes: [
+            'dark',
+            'bright',
+            'custom',
+          ],
+          properties: [
+            'background',
+            'border',
+            'appendBG',
+            'hoverBG',
+            'master',
+            'slave',
+            'attention',
+          ],
+        },
+        server: {
+          schemes: [
+            'dark',
+            'bright',
+            'custom',
+          ],
+          properties: [
+            'heading',
+            'master',
+            'slave',
+            'attention',
+          ],
+        },
+      };
+
+      Object.keys(schemePattern).forEach((section) => {
+        Describe(Utility.method(`Checking "${section}" schemes:`), () => {
+          It(`env "${section}"`, () => {
+            colorSchemes.should.have.property(section);
+          });
+          schemePattern[section].schemes.forEach((scheme) => {
+            It(`scheme "${scheme}"`, () => {
+              colorSchemes[section].should.have.property(scheme);
+            });
+            Describe(`${scheme}:`, () => {
+              schemePattern[section].properties.forEach((property) => {
+                It(`${property}`, () => {
+                  colorSchemes[section][scheme].should.have.property(property);
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
