e0a3b2086fa58b68009e01fa5270d9145acee0f6
mike_shu <heliax44@gmail.com>
2017-12-12 04:36:39 +0400

    Added Client-app file.

--- app/client-app.js
+++ app/client-app.js
@@ -1,1 +1,61 @@
-console.log('Initializing the project.');
+const Utility = require('./misc');
+
+const Debug = new UduJS({
+  maxWidth: 'auto',
+  showClearTitle: false,
+});
+const allTypes = Utility.allTypesObject;
+const someCode = Utility.doSomeCode;
+const party = Utility.multiObject;
+
+// View the value of some variable.
+Debug.log(party, 'Friday\'s party.');
+
+// Measurement with control points.
+Debug.rttPoint();
+someCode();
+Debug.rttPoint('Some code was executed.');
+someCode();
+Debug.rttPoint('More code.');
+
+// A single measurement.
+Debug.rttStart('Single testing of some code.');
+someCode();
+Debug.rttFinish();
+
+// A single measurement with the calculation of the average result.
+Debug.rttAverage(someCode, 10, 'The average execution time of some code.', true);
+
+// A pop-up message with a value of some variable.
+Debug.popup('Some variable.');
+
+// The message either to the console or to a pop-up message (specified in the config).
+Debug.show(party, 'Friday\'s party.');
+
+// A fixed field in a pop-up message with debugging information.
+const someVariable = 123;
+Debug.observer(someVariable);
+
+// An example of interaction with the interface.
+let numberOfClicks = 0;
+$(document).ready(() => {
+  $('button#addTestString').on('mouseover', () => {
+    Debug.observer(`Number of clicks: ${numberOfClicks}`);
+  }).on('mouseleave', () => {
+    Debug.observer('The add button has lost focus.');
+  }).on('click', () => {
+    Debug.show(`${$('textarea#textField').val()} #${numberOfClicks += 1}`);
+  });
+  $('button#addTestObject').on('click', () => {
+    Debug.show(allTypes);
+    numberOfClicks += 1;
+  });
+  $('button#toConsole').on('click', () => {
+    Debug.log(allTypes);
+    numberOfClicks += 1;
+  });
+  $('button#clearPopup').on('click', () => {
+    Debug.popupReset();
+    numberOfClicks += 1;
+  });
+});
